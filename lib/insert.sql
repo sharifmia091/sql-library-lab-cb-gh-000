6a13e3363aa0921e84b6c0c76f246a3ef8d577dc
md mia <sharifmia091@gmail.com>
2018-08-30 06:34:42 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO series (id) VALUES (1), (2);
+INSERT INTO books (id) VALUES (1), (2), (3), (4), (5), (6);
+INSERT INTO characters (id) VALUES (1), (2), (3), (4), (5), (6), (7), (8);
+INSERT INTO subgenres (id) VALUES (1), (2);
+INSERT INTO authors (id) VALUES (1), (2);
+INSERT INTO character_books (id) VALUES (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), (11), (12), (13), (14), (15), (16);
